a1b5763ae7f340630d8cf9ed0d99f8a3bd6197d6
Dass7550 <dass7550@gmail.com>
2022-11-12 12:00:39 +0600

    everything is done with some error

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,24 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import '../shop/Shop.css'
+
+
+
 const Shop = () => {
     const [products, setProducts] = useState([])
+
     useEffect(() => {
         fetch("products.json")
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data)
+
+            });
     }, [])
 
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
 
+        }
+        setCart(savedCart);
+    }, [products])
 
     const [cart, setCart] = useState([]);
 
-    function addToCart(product) {
-        console.log(product);
-        const newCart = [...cart, product];
+    function addToCart(SelectedProduct) {
+
+        const exist = cart.find(product => product.id === SelectedProduct.id)
+        let newCart = [];
+
+        if (!exist) {
+            SelectedProduct.quantity = 1;
+            newCart = [...cart, SelectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== SelectedProduct.id);
+            exist.quantity = exist.quantity + 1;
+            newCart = [...cart, exist];
+        }
+
         setCart(newCart)
-        console.log(cart);
+        addToDb(SelectedProduct.id)
+        //console.log(cart);
     }
+
+
+
     return (
         <div className="Main-Section">
             <div className="Shopping-area">
@@ -26,9 +64,7 @@ const Shop = () => {
                     products.map(product => <Product product={product} addToCart={addToCart}></Product>)
                 }
             </div>
-            <div className="Cart-Area">
-                <h1>Cart area{cart.length}</h1>
-            </div>
+            <Cart cart={cart}></Cart>
         </div>
     );
 };
